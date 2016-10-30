f803bf66bfafdc78d60e47ac59a05656d0cec548
wowdog <can0any@daum.net>
2016-10-30 20:08:52 +0900

    bower없앰

--- app.js
+++ app.js
@@ -57,8 +57,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.listen(process.env.PORT || 8080, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 module.exports = app;
